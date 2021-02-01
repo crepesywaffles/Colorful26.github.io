49285102becb0bf06c53c3b66c6908633591986a
Camilo Andres Gonzalez Mendivelso <camilo.gonzalez@crepes.ofi>
2021-02-01 14:14:06 -0500

    remove icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,9 +9,6 @@ module.exports = {
     "gatsby-plugin-offline",
     {
       resolve: "gatsby-plugin-manifest",
-      options: {
-        icon: "src/images/icon.png",
-      },
     },
     "gatsby-transformer-sharp",
     {
